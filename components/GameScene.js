13d8339500a6ba94432d422cbdb05819f61d2ba5
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-07 11:07:47 -0600

    Update GameScene to use new Character component

--- components/GameScene.js
+++ components/GameScene.js
@@ -1,27 +1,15 @@
-import React, { useRef } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
+import React, { Suspense } from 'react';
+import { Canvas, useLoader } from '@react-three/fiber';
 import { OrthographicCamera } from '@react-three/drei';
-
-const Fighter = ({ position, color }) => {
-  const mesh = useRef();
-
-  useFrame(() => {
-    mesh.current.rotation.z += 0.01;
-  });
-
-  return (
-    <mesh position={position} ref={mesh}>
-      <planeGeometry args={[1, 2]} />
-      <meshBasicMaterial color={color} />
-    </mesh>
-  );
-};
+import { TextureLoader } from 'three';
+import Character from './Character';
 
 const Background = () => {
+  const texture = useLoader(TextureLoader, '/background.jpg');
   return (
     <mesh position={[0, 0, -1]}>
       <planeGeometry args={[16, 9]} />
-      <meshBasicMaterial color="#87CEEB" />
+      <meshBasicMaterial map={texture} />
     </mesh>
   );
 };
@@ -29,12 +17,25 @@ const Background = () => {
 const GameScene = () => {
   return (
     <Canvas>
-      <OrthographicCamera makeDefault zoom={50} position={[0, 0, 5]} />
+      <OrthographicCamera makeDefault position={[0, 0, 5]} zoom={100} />
       <ambientLight intensity={0.5} />
       <pointLight position={[10, 10, 10]} />
-      <Background />
-      <Fighter position={[-3, 0, 0]} color="red" />
-      <Fighter position={[3, 0, 0]} color="blue" />
+      
+      <Suspense fallback={null}>
+        <Background />
+        <Character 
+          position={[-2, 0, 0]} 
+          spriteSheet="/character1_spritesheet.png"
+          frameSize={[1, 2]}
+          totalFrames={4}
+        />
+        <Character 
+          position={[2, 0, 0]} 
+          spriteSheet="/character2_spritesheet.png"
+          frameSize={[1, 2]}
+          totalFrames={4}
+        />
+      </Suspense>
     </Canvas>
   );
 };
