ec913560953a9ea87c8b308d1aefca0b98907017
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-07 16:21:05 -0600

    Created StartScreen component with attractive design and game instructions

--- components/StartScreen.js
+++ components/StartScreen.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import styles from '../styles/StartScreen.module.css';
 
-const StartScreen = ({ onStartGame, onOpenOptions }) => {
+/**
+ * @component StartScreen
+ * @description Displays the start screen for the "Burger vs. Jean" game
+ * @param {Object} props - Component props
+ * @param {function} props.onStartGame - Function to call when the start game button is clicked
+ */
+const StartScreen = ({ onStartGame }) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.startScreen}>
       <h1 className={styles.title}>Burger vs. Jean</h1>
-      <button className={`${styles.button} ${styles.startButton}`} onClick={onStartGame}>
-        Start Game
-      </button>
-      <button className={`${styles.button} ${styles.optionsButton}`} onClick={onOpenOptions}>
-        Options
-      </button>
+      <p className={styles.subtitle}>The ultimate food fight!</p>
+      <button className={styles.startButton} onClick={onStartGame}>Start Game</button>
+      <div className={styles.instructions}>
+        <h2>How to Play:</h2>
+        <ul>
+          <li>Use A, S, D keys for Burger's attacks</li>
+          <li>Use J, K, L keys for Jean's attacks</li>
+          <li>Defeat your opponent to win!</li>
+        </ul>
+      </div>
     </div>
   );
 };
