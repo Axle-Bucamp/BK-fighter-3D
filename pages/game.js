b8bfbf26992a76a61c12512f531127d76d056210
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-07 11:06:51 -0600

    Create game page with GameScene component

--- pages/game.js
+++ pages/game.js
@@ -1,8 +1,14 @@
-export default function Game() {
+import React from 'react';
+import dynamic from 'next/dynamic';
+
+const GameScene = dynamic(() => import('../components/GameScene'), { ssr: false });
+
+const GamePage = () => {
   return (
-    <div>
-      <h1>Game Scene</h1>
-      {/* Add your game components here */}
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <GameScene />
     </div>
   );
-}
+};
+
+export default GamePage;
