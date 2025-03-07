72a6e4f61e462c7e2ef1c54201f7a85efb87b7eb
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-07 11:11:27 -0600

    Update game page to use selected characters

--- pages/game.js
+++ pages/game.js
@@ -1,14 +1,19 @@
-import React from 'react';
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
 
 const GameScene = dynamic(() => import('../components/GameScene'), { ssr: false });
 
-const GamePage = () => {
+export default function GamePage() {
+  const router = useRouter();
+  const { player1, player2 } = router.query;
+
+  if (!player1 || !player2) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <GameScene />
+      <GameScene player1Character={player1} player2Character={player2} />
     </div>
   );
-};
-
-export default GamePage;
+}
