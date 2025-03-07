3a9a929b1352cf099fcbe1d3802e6316b0d4439c
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-07 14:28:48 -0600

    Update StartScreen.js to use new CSS module

--- components/StartScreen.js
+++ components/StartScreen.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import styles from './StartScreen.module.css';
+import styles from '../styles/StartScreen.module.css';
 
 const StartScreen = ({ onStartGame, onOpenOptions }) => {
   return (
-    <div className={styles.startScreen}>
-      <h1 className={styles.title}>Platformer Battle</h1>
-      <button className={styles.button} onClick={onStartGame}>Start Game</button>
-      <button className={styles.button} onClick={onOpenOptions}>Options</button>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Burger vs. Jean</h1>
+      <button className={`${styles.button} ${styles.startButton}`} onClick={onStartGame}>
+        Start Game
+      </button>
+      <button className={`${styles.button} ${styles.optionsButton}`} onClick={onOpenOptions}>
+        Options
+      </button>
     </div>
   );
 };
