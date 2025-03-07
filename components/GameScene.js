6cd46a065994ac1f6bef16b704be90931abfe8dc
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-07 11:11:09 -0600

    Update GameScene to use selected characters

--- components/GameScene.js
+++ components/GameScene.js
@@ -4,111 +4,110 @@ import { OrthographicCamera, Text } from '@react-three/drei';
 import * as THREE from 'three';
 import { useGameStore, useKeyPress } from '../lib/gameLogic';
 
-const Character = ({ position, isAttacking, spriteSheet }) => {
-  const texture = useLoader(THREE.TextureLoader, spriteSheet);
+function Background() {
+  const texture = useLoader(THREE.TextureLoader, '/background.jpg');
+  return (
+    <mesh position={[0, 0, -1]}>
+      <planeGeometry args={[16, 9]} />
+      <meshBasicMaterial map={texture} />
+    </mesh>
+  );
+}
+
+function Character({ position, spritesheet, direction, attacking }) {
+  const texture = useLoader(THREE.TextureLoader, spritesheet);
   const ref = useRef();
 
   useFrame(({ clock }) => {
-    const t = clock.getElapsedTime();
-    const frame = Math.floor(t * 10) % 4;
-    ref.current.material.map.offset.x = frame / 4;
+    if (ref.current) {
+      const elapsedTime = clock.getElapsedTime();
+      const frame = Math.floor(elapsedTime * 10) % 4;
+      ref.current.material.map.offset.x = frame / 4;
+    }
   });
 
   return (
-    <sprite ref={ref} position={[position, 0, 0]} scale={[1, 2, 1]}>
+    <sprite ref={ref} position={position} scale={[2, 2, 1]}>
       <spriteMaterial
-        map={texture}
         transparent
-        opacity={isAttacking ? 0.5 : 1}
+        map={texture}
+        opacity={attacking ? 0.7 : 1}
       />
     </sprite>
   );
-};
+}
 
-const Background = () => {
-  const texture = useLoader(THREE.TextureLoader, '/background.jpg');
-  return (
-    <sprite scale={[10, 5, 1]} position={[0, 0, -1]}>
-      <spriteMaterial map={texture} />
-    </sprite>
-  );
-};
-
-const HealthBar = ({ position, health }) => {
+function HealthBar({ position, health }) {
   return (
     <group position={position}>
-      <mesh scale={[2, 0.2, 1]}>
-        <planeGeometry />
-        <meshBasicMaterial color="gray" />
-      </mesh>
-      <mesh scale={[health / 50, 0.2, 1]} position={[(health / 100 - 1), 0, 0.1]}>
-        <planeGeometry />
+      <mesh position={[0, 0, 0]}>
+        <planeGeometry args={[2, 0.2]} />
         <meshBasicMaterial color="red" />
+      </mesh>
+      <mesh position={[-1 + health / 100, 0, 0.1]}>
+        <planeGeometry args={[2 * (health / 100), 0.2]} />
+        <meshBasicMaterial color="green" />
       </mesh>
     </group>
   );
-};
+}
 
-const GameScene = () => {
-  const { player1, player2, movePlayer, attack, resetAttack } = useGameStore();
-
-  const aPressed = useKeyPress('a');
-  const hPressed = useKeyPress('h');
-  const jPressed = useKeyPress('j');
-  const kPressed = useKeyPress('k');
+export function GameScene({ player1Character, player2Character }) {
+  const { characters, moveCharacter, attack, resetAttack, setCharacter } = useGameStore();
 
   useEffect(() => {
-    if (aPressed) movePlayer('player1', -1);
-    if (hPressed) movePlayer('player1', 1);
-    if (jPressed) movePlayer('player2', -1);
-    if (kPressed) movePlayer('player2', 1);
-  }, [aPressed, hPressed, jPressed, kPressed]);
+    setCharacter('player1', player1Character);
+    setCharacter('player2', player2Character);
+  }, [player1Character, player2Character, setCharacter]);
 
-  useEffect(() => {
-    const handleKeyDown = (e) => {
-      if (e.key === 'd') attack('player1');
-      if (e.key === 'l') attack('player2');
-    };
+  // Player 1 controls
+  const leftPressed = useKeyPress('a');
+  const rightPressed = useKeyPress('d');
+  const attackPressed = useKeyPress('s');
 
-    const handleKeyUp = (e) => {
-      if (e.key === 'd') resetAttack('player1');
-      if (e.key === 'l') resetAttack('player2');
-    };
+  // Player 2 controls
+  const leftPressed2 = useKeyPress('j');
+  const rightPressed2 = useKeyPress('l');
+  const attackPressed2 = useKeyPress('k');
 
-    window.addEventListener('keydown', handleKeyDown);
-    window.addEventListener('keyup', handleKeyUp);
+  useFrame(() => {
+    if (leftPressed) moveCharacter('player1', -1);
+    if (rightPressed) moveCharacter('player1', 1);
+    if (attackPressed) attack('player1');
+    else resetAttack('player1');
 
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-      window.removeEventListener('keyup', handleKeyUp);
-    };
-  }, []);
+    if (leftPressed2) moveCharacter('player2', -1);
+    if (rightPressed2) moveCharacter('player2', 1);
+    if (attackPressed2) attack('player2');
+    else resetAttack('player2');
+  });
 
   return (
     <Canvas>
-      <OrthographicCamera makeDefault position={[0, 0, 5]} />
+      <OrthographicCamera makeDefault position={[0, 0, 5]} zoom={100} />
       <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} />
       <Background />
       <Character
-        position={player1.position}
-        isAttacking={player1.isAttacking}
-        spriteSheet="/character1_spritesheet.png"
+        position={[characters.player1.x / 50 - 8, characters.player1.y / 50 - 4.5, 0]}
+        spritesheet={`/${characters.player1.character}_spritesheet.png`}
+        direction={characters.player1.direction}
+        attacking={characters.player1.attacking}
       />
       <Character
-        position={player2.position}
-        isAttacking={player2.isAttacking}
-        spriteSheet="/character2_spritesheet.png"
+        position={[characters.player2.x / 50 - 8, characters.player2.y / 50 - 4.5, 0]}
+        spritesheet={`/${characters.player2.character}_spritesheet.png`}
+        direction={characters.player2.direction}
+        attacking={characters.player2.attacking}
       />
-      <HealthBar position={[-4, 2, 0]} health={player1.health} />
-      <HealthBar position={[4, 2, 0]} health={player2.health} />
-      <Text position={[-4, 2.2, 0]} fontSize={0.2} color="white">
+      <HealthBar position={[-7, 4, 0]} health={characters.player1.health} />
+      <HealthBar position={[7, 4, 0]} health={characters.player2.health} />
+      <Text position={[-7, 4.2, 0]} fontSize={0.5} color="white">
         Player 1
       </Text>
-      <Text position={[4, 2.2, 0]} fontSize={0.2} color="white">
+      <Text position={[7, 4.2, 0]} fontSize={0.5} color="white">
         Player 2
       </Text>
     </Canvas>
   );
-};
-
-export default GameScene;
+}
