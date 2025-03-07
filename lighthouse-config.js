f553e7c6f977f843c7799974399d9b75812e4bf5
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-07 16:43:12 -0600

    Create Lighthouse configuration file

---
+++ lighthouse-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: 'lighthouse:default',
+  settings: {
+    onlyCategories: ['performance', 'accessibility', 'best-practices', 'seo'],
+    formFactor: 'desktop',
+    throttling: {
+      rttMs: 40,
+      throughputKbps: 10 * 1024,
+      cpuSlowdownMultiplier: 1,
+    },
+    screenEmulation: {
+      mobile: false,
+      width: 1350,
+      height: 940,
+      deviceScaleFactor: 1,
+      disabled: false,
+    },
+    skipAudits: ['uses-http2'],
+  },
+}
