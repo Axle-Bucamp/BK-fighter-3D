c7294a7de8e72b2dceb0bbf80bbd9c8897b4480e
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-09 03:29:19 -0500

    Add BurgerKingClassic character class

--- components/BurgerKingClassic.js
+++ components/BurgerKingClassic.js
@@ -2,15 +2,14 @@ import { Character } from '../lib/Character';
 
 export class BurgerKingClassic extends Character {
   constructor(world, x, y) {
-    super(world, x, y, 60, 120); // Adjust size as needed
-    this.name = "Classic Burger King";
-    this.specialAbility = "Whopper Toss";
+    super(world, x, y, 50, 80, { label: 'BurgerKingClassic' });
+    this.name = 'Burger King Classic';
     this.strength = 8;
     this.speed = 7;
   }
 
-  useSpecialAbility() {
-    console.log(`${this.name} uses ${this.specialAbility}!`);
+  specialAbility() {
+    console.log('Whopper Smash: Burger King Classic unleashes a powerful smash attack!');
     // Implement special ability logic here
   }
 }
