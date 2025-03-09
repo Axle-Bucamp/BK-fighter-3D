8e219686d256c32ce4ad84357be720fc3756e096
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-09 05:07:45 -0500

    Create constants file

--- src/game/constants.js
+++ src/game/constants.js
@@ -1,15 +1,21 @@
-export const GAME_STATES = {
-  MENU: 'menu',
-  GAME: 'game',
-  TUTORIAL: 'tutorial',
+export const GAME_MODES = {
+  TIMED: 'TIMED',
+  ENDLESS: 'ENDLESS',
 };
 
 export const CHARACTERS = {
-  BURGER: 'burger',
-  JEAN: 'jean',
+  BURGER_KING_CHICKEN: 'Burger King Chicken',
+  BURGER_KING_CLASSIC: 'Burger King Classic',
+  BURGER_KING_WHOPPER: 'Burger King Whopper',
+  PIZZA_CHARACTER: 'Pizza Character',
+  TRIPO_CHARACTER: 'Tripo Character',
+  VAN_DAMME_KICKBOXER: 'Van Damme Kickboxer',
+  VAN_DAMME_TIMECOP: 'Van Damme Timecop',
+  VAN_DAMME_UNIVERSAL_SOLDIER: 'Van Damme Universal Soldier',
 };
 
 export const ARENAS = {
-  CITY: 'city',
-  BEACH: 'beach',
+  FAST_FOOD_ALLEY: 'Fast Food Alley',
+  PIZZA_PARLOR: 'Pizza Parlor',
+  MOVIE_SET: 'Movie Set',
 };
