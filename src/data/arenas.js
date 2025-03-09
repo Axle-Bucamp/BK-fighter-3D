c33be890b4ea16736ddf347bec7c5012d7bdb186
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-09 03:42:32 -0500

    Add arenas.js with new fighting arenas

---
+++ src/data/arenas.js
@@ -0,0 +1,22 @@
+const arenas = [
+  {
+    id: 'burger_joint',
+    name: 'Burger Joint',
+    description: 'A classic fast-food restaurant setting',
+    backgroundImage: '/images/burger_joint_bg.jpg',
+  },
+  {
+    id: 'neon_city',
+    name: 'Neon City',
+    description: 'A futuristic cyberpunk-themed arena with neon lights and towering skyscrapers',
+    backgroundImage: '/images/neon_city_bg.jpg',
+  },
+  {
+    id: 'ancient_ruins',
+    name: 'Ancient Ruins',
+    description: 'A mystical arena set among the crumbling remains of an ancient civilization',
+    backgroundImage: '/images/ancient_ruins_bg.jpg',
+  },
+];
+
+export default arenas;
