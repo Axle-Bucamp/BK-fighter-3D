c8a1610ebca3edc0db93b0c22bb9671a889bf413
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-09 11:34:03 -0500

    Update main.tf for Terraform configuration

--- main.tf
+++ main.tf
@@ -2,84 +2,32 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support = true
-
-  tags = {
-    Name = "bk-fighter-3d-vpc"
-  }
-}
-
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "bk-fighter-3d-igw"
-  }
-}
-
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
-
-  tags = {
-    Name = "bk-fighter-3d-subnet"
-  }
-}
-
-resource "aws_security_group" "allow_web" {
-  name        = "allow_web_traffic"
-  description = "Allow inbound web traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_web"
-  }
-}
-
 resource "aws_instance" "app_server" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main.id
-  vpc_security_group_ids = [aws_security_group.allow_web.id]
 
   tags = {
-    Name = "BK-fighter-3D-AppServer"
+    Name = "BK-Fighter-3D-Server"
   }
 }
 
-resource "aws_eip" "app_server_eip" {
-  instance = aws_instance.app_server.id
-  vpc      = true
+resource "aws_db_instance" "mongodb" {
+  engine         = "mongodb"
+  engine_version = "4.4"
+  instance_class = "db.t3.micro"
+  name           = "bkfighter"
+  username       = "admin"
+  password       = "password" # Change this to a secure password
+
+  tags = {
+    Name = "BK-Fighter-3D-MongoDB"
+  }
 }
 
-output "public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_eip.app_server_eip.public_ip
+output "app_server_public_ip" {
+  value = aws_instance.app_server.public_ip
+}
+
+output "mongodb_endpoint" {
+  value = aws_db_instance.mongodb.endpoint
 }
