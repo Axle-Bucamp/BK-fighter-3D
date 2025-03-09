9e583ddab59c70c487bb13b2833455b6554a3ba9
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-09 11:41:26 -0500

    Add main.tf file with Terraform configuration

--- main.tf
+++ main.tf
@@ -8,11 +8,11 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-west-2"  # or your preferred region
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI (HVM), SSD Volume Type
   instance_type = "t2.micro"
 
   tags = {
@@ -20,25 +20,30 @@ resource "aws_instance" "app_server" {
   }
 }
 
-resource "aws_db_instance" "mongodb" {
-  engine               = "mongodb"
-  engine_version       = "5.0"
-  instance_class       = "db.t3.micro"
-  allocated_storage    = 20
-  identifier           = "bk-fighter-3d-db"
-  username             = "admin"
-  password             = var.db_password
-  skip_final_snapshot  = true
-}
-
-resource "aws_security_group" "allow_http" {
-  name        = "allow_http"
-  description = "Allow HTTP inbound traffic"
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow inbound web traffic"
 
   ingress {
-    description = "HTTP from anywhere"
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Custom App Port"
+    from_port   = 5000
+    to_port     = 5000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -49,17 +54,30 @@ resource "aws_security_group" "allow_http" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "allow_web"
+  }
 }
 
-variable "db_password" {
-  description = "Password for the database"
-  type        = string
+resource "aws_db_instance" "mongodb" {
+  identifier           = "bkfighter-mongodb"
+  engine               = "mongodb"
+  engine_version       = "5.0"
+  instance_class       = "db.t3.micro"
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  username             = "admin"
+  password             = "changeme"  # Change this and use a secret manager in production
+  parameter_group_name = "default.mongodb5.0"
+  skip_final_snapshot  = true
+
+  tags = {
+    Name = "BK-Fighter-3D-MongoDB"
+  }
 }
 
 output "app_server_public_ip" {
-  value = aws_instance.app_server.public_ip
-}
-
-output "mongodb_endpoint" {
-  value = aws_db_instance.mongodb.endpoint
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
 }
