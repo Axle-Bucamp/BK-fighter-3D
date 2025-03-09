5acb6182c2a09c27d0d372751d29ab1a36db12e4
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-09 03:30:49 -0500

    Add VanDammeTimecop character class

--- components/VanDammeTimecop.js
+++ components/VanDammeTimecop.js
@@ -2,15 +2,14 @@ import { Character } from '../lib/Character';
 
 export class VanDammeTimecop extends Character {
   constructor(world, x, y) {
-    super(world, x, y, 60, 120);
-    this.name = "Timecop";
-    this.specialAbility = "Time Warp";
+    super(world, x, y, 52, 86, { label: 'VanDammeTimecop' });
+    this.name = 'Van Damme Timecop';
     this.strength = 8;
     this.speed = 9;
   }
 
-  useSpecialAbility() {
-    console.log(`${this.name} uses ${this.specialAbility}!`);
+  specialAbility() {
+    console.log('Time Warp: Van Damme Timecop manipulates time to gain an advantage!');
     // Implement special ability logic here
   }
 }
