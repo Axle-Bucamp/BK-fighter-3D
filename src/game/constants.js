71d41cf6551385db333ca7db67a4e54a63d9726e
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-09 03:47:52 -0500

    Add game constants

---
+++ src/game/constants.js
@@ -0,0 +1,15 @@
+export const GAME_STATES = {
+  MENU: 'menu',
+  GAME: 'game',
+  TUTORIAL: 'tutorial',
+};
+
+export const CHARACTERS = {
+  BURGER: 'burger',
+  JEAN: 'jean',
+};
+
+export const ARENAS = {
+  CITY: 'city',
+  BEACH: 'beach',
+};
