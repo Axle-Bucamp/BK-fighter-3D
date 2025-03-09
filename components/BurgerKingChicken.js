8b99af64d4f0b98887e3cf69c5dcabfe6e35d60e
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-09 03:29:54 -0500

    Add BurgerKingChicken character class

--- components/BurgerKingChicken.js
+++ components/BurgerKingChicken.js
@@ -2,15 +2,14 @@ import { Character } from '../lib/Character';
 
 export class BurgerKingChicken extends Character {
   constructor(world, x, y) {
-    super(world, x, y, 55, 110); // Slightly smaller
-    this.name = "Chicken Royale";
-    this.specialAbility = "Crispy Combo";
+    super(world, x, y, 45, 75, { label: 'BurgerKingChicken' });
+    this.name = 'Burger King Chicken';
     this.strength = 7;
     this.speed = 9;
   }
 
-  useSpecialAbility() {
-    console.log(`${this.name} uses ${this.specialAbility}!`);
+  specialAbility() {
+    console.log('Chicken Frenzy: Burger King Chicken unleashes a flurry of rapid attacks!');
     // Implement special ability logic here
   }
 }
