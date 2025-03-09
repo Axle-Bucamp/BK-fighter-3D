a2753d48393f67fe456b5cda28a90e7f558f3688
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-09 03:29:36 -0500

    Add BurgerKingWhopper character class

--- components/BurgerKingWhopper.js
+++ components/BurgerKingWhopper.js
@@ -2,15 +2,14 @@ import { Character } from '../lib/Character';
 
 export class BurgerKingWhopper extends Character {
   constructor(world, x, y) {
-    super(world, x, y, 70, 130); // Slightly larger
-    this.name = "Whopper King";
-    this.specialAbility = "Flame Grilled Rush";
+    super(world, x, y, 60, 90, { label: 'BurgerKingWhopper' });
+    this.name = 'Burger King Whopper';
     this.strength = 10;
     this.speed = 6;
   }
 
-  useSpecialAbility() {
-    console.log(`${this.name} uses ${this.specialAbility}!`);
+  specialAbility() {
+    console.log('Flame Grilled Fury: Burger King Whopper ignites with flame-grilled power!');
     // Implement special ability logic here
   }
 }
