37915cebd8e3b8fbda9109262525fabed92528b5
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-09 11:25:25 -0500

    Update server.js to use config and connect to MongoDB

--- server/server.js
+++ server/server.js
@@ -2,102 +2,55 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const mongoose = require('mongoose');
-
-const MatchmakingService = require('./services/matchmaking');
-const LobbyService = require('./services/lobby');
+const config = require('../config');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
-
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/fightinggame', { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Middleware
-app.use(express.json());
-
-// Routes
-app.get('/', (req, res) => {
-  res.send('Fighting Game Server');
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 });
 
-// Socket.IO connection handling
+mongoose.connect(config.mongoDbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
+
+const LobbyService = require('./services/LobbyService');
+const MatchmakingService = require('./services/MatchmakingService');
+
+const lobbyService = new LobbyService(io);
+const matchmakingService = new MatchmakingService(io);
+
 io.on('connection', (socket) => {
   console.log('New client connected');
 
-  let currentPlayer = null;
-
-  socket.on('register', (username) => {
-    currentPlayer = { id: socket.id, username, socket };
-    socket.emit('registered', { id: currentPlayer.id, username });
+  socket.on('joinLobby', (username) => {
+    lobbyService.addPlayer(socket.id, username);
   });
 
-  socket.on('joinMatchmaking', () => {
-    if (currentPlayer) {
-      MatchmakingService.addToQueue(currentPlayer);
-    }
+  socket.on('leaveLobby', () => {
+    lobbyService.removePlayer(socket.id);
   });
 
-  socket.on('leaveMatchmaking', () => {
-    if (currentPlayer) {
-      MatchmakingService.removeFromQueue(currentPlayer);
-    }
+  socket.on('startMatchmaking', () => {
+    matchmakingService.addToQueue(socket.id);
   });
 
-  socket.on('acceptMatch', (matchId) => {
-    if (currentPlayer) {
-      MatchmakingService.acceptMatch(matchId, currentPlayer.id);
-    }
-  });
-
-  socket.on('createLobby', () => {
-    if (currentPlayer) {
-      const lobbyId = LobbyService.createLobby(currentPlayer);
-      socket.emit('lobbyCreated', { lobbyId });
-    }
-  });
-
-  socket.on('joinLobby', (lobbyId) => {
-    if (currentPlayer) {
-      const joined = LobbyService.joinLobby(lobbyId, currentPlayer);
-      if (joined) {
-        socket.emit('lobbyJoined', { lobbyId });
-      } else {
-        socket.emit('lobbyJoinFailed', { lobbyId });
-      }
-    }
-  });
-
-  socket.on('leaveLobby', (lobbyId) => {
-    if (currentPlayer) {
-      LobbyService.leaveLobby(lobbyId, currentPlayer.id);
-      socket.emit('lobbyLeft', { lobbyId });
-    }
-  });
-
-  socket.on('getLobbies', () => {
-    const lobbies = LobbyService.getAvailableLobbies();
-    socket.emit('lobbiesList', lobbies);
-  });
-
-  socket.on('startGame', (lobbyId) => {
-    if (currentPlayer) {
-      const started = LobbyService.startGame(lobbyId);
-      if (started) {
-        io.to(lobbyId).emit('gameStarted', { lobbyId });
-      }
-    }
+  socket.on('cancelMatchmaking', () => {
+    matchmakingService.removeFromQueue(socket.id);
   });
 
   socket.on('disconnect', () => {
     console.log('Client disconnected');
-    if (currentPlayer) {
-      MatchmakingService.removeFromQueue(currentPlayer);
-    }
+    lobbyService.removePlayer(socket.id);
+    matchmakingService.removeFromQueue(socket.id);
   });
 });
 
-const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
+});
 
-module.exports = { app, io };
+module.exports = server;
