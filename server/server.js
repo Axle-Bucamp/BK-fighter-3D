1f7204364cf7ffe9bd50d46be0faf88c2f262c63
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-09 03:36:52 -0500

    Set up basic server structure

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const mongoose = require('mongoose');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/fightinggame', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Fighting Game Server');
+});
+
+// Socket.IO connection handling
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = { app, io };
