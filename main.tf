5ed69627ca92625b50f0add9a0cc99f40f8c1f0e
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-09 11:31:07 -0500

    Update Terraform script for AWS infrastructure

--- main.tf
+++ main.tf
@@ -2,62 +2,51 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_ecr_repository" "bk_fighter_3d" {
-  name = "bk-fighter-3d"
-}
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
 
-resource "aws_ecs_cluster" "bk_fighter_3d" {
-  name = "bk-fighter-3d-cluster"
-}
-
-resource "aws_ecs_task_definition" "bk_fighter_3d" {
-  family                   = "bk-fighter-3d"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-
-  container_definitions = jsonencode([{
-    name  = "bk-fighter-3d"
-    image = "${aws_ecr_repository.bk_fighter_3d.repository_url}:latest"
-    portMappings = [{
-      containerPort = 80
-      hostPort      = 80
-    }]
-  }])
-}
-
-resource "aws_ecs_service" "bk_fighter_3d" {
-  name            = "bk-fighter-3d-service"
-  cluster         = aws_ecs_cluster.bk_fighter_3d.id
-  task_definition = aws_ecs_task_definition.bk_fighter_3d.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    assign_public_ip = true
-    security_groups  = [aws_security_group.ecs_tasks.id]
-    subnets          = [aws_subnet.main.id]
+  tags = {
+    Name = "bk-fighter-3d-vpc"
   }
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "bk-fighter-3d-igw"
+  }
 }
 
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "bk-fighter-3d-subnet"
+  }
 }
 
-resource "aws_security_group" "ecs_tasks" {
-  name        = "bk-fighter-3d-sg"
-  description = "Allow inbound traffic"
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow inbound web traffic"
   vpc_id      = aws_vpc.main.id
 
   ingress {
+    description = "HTTP"
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -68,4 +57,29 @@ resource "aws_security_group" "ecs_tasks" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.allow_web.id]
+
+  tags = {
+    Name = "BK-fighter-3D-AppServer"
+  }
+}
+
+resource "aws_eip" "app_server_eip" {
+  instance = aws_instance.app_server.id
+  vpc      = true
+}
+
+output "public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_eip.app_server_eip.public_ip
 }
