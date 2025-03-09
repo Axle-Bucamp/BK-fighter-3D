fb259bb179fd9171a320167cfac9f5d70ba1bbbb
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-09 16:46:43 -0500

    Update multiplayerManager.js to use ES module syntax

--- src/multiplayerManager.js
+++ src/multiplayerManager.js
@@ -1,38 +1,15 @@
-import io from 'socket.io-client';
-
-class MultiplayerManager {
+export default class MultiplayerManager {
   constructor(gameEngine) {
     this.gameEngine = gameEngine;
-    this.socket = null;
   }
 
   connect(serverUrl) {
-    this.socket = io(serverUrl);
-
-    this.socket.on('connect', () => {
-      console.log('Connected to server');
-    });
-
-    this.socket.on('disconnect', () => {
-      console.log('Disconnected from server');
-    });
-
-    // Add more socket event listeners here
-  }
-
-  joinGame(gameId) {
-    if (this.socket) {
-      this.socket.emit('joinGame', { gameId });
-    }
+    // Implement WebSocket connection logic
   }
 
   leaveGame() {
-    if (this.socket) {
-      this.socket.emit('leaveGame');
-    }
+    // Implement disconnect logic
   }
 
-  // Add more multiplayer-related methods here
+  // Add other multiplayer methods
 }
-
-export default MultiplayerManager;
