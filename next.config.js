e684c45bc47f33bf8ddaeffc97daeb0954d3d879
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-09 18:12:51 -0500

    Update next.config.js to allow loading of 3D model files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 module.exports = {
-  reactStrictMode: true,
-  experimental: {
-    esmExternals: true
-  }
-}
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glb|gltf|fbx)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/images',
+          outputPath: 'static/images/',
+        },
+      },
+    });
+
+    return config;
+  },
+};
