cf0f040e249465f2b3bf0d21437927c7eeaedc66
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-09 05:10:12 -0500

    Create basic Menu component

--- components/Menu.js
+++ components/Menu.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import styles from '../styles/Menu.module.css';
 
-const Menu = ({ onStartSinglePlayer, onStartTwoPlayer, onShowTutorial }) => {
+const Menu = ({ onStartSinglePlayer, onStartTwoPlayer, onShowOptions, onShowTutorial }) => {
   return (
     <div className={styles.menuContainer}>
-      <h1 className={styles.title}>Burger vs. Jean</h1>
-      <div className={styles.menuOptions}>
+      <h1 className={styles.title}>Burger King Fighter</h1>
+      <nav className={styles.menuNav}>
         <button className={styles.menuButton} onClick={onStartSinglePlayer}>
           Single Player
         </button>
         <button className={styles.menuButton} onClick={onStartTwoPlayer}>
           Two Player
         </button>
+        <button className={styles.menuButton} onClick={onShowOptions}>
+          Options
+        </button>
         <button className={styles.menuButton} onClick={onShowTutorial}>
           Tutorial
         </button>
-      </div>
+      </nav>
     </div>
   );
 };
