496d45afab1b147276b626aee0504db4cfa06f1f
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-09 16:45:20 -0500

    Create src/multiplayerManager.js with ES module syntax

--- src/multiplayerManager.js
+++ src/multiplayerManager.js
@@ -1,93 +1,38 @@
 import io from 'socket.io-client';
-import config from '../config';
 
 class MultiplayerManager {
   constructor(gameEngine) {
     this.gameEngine = gameEngine;
     this.socket = null;
-    this.lobby = null;
   }
 
-  connect() {
-    this.socket = io(config.serverUrl || 'http://localhost:3000');
+  connect(serverUrl) {
+    this.socket = io(serverUrl);
 
     this.socket.on('connect', () => {
       console.log('Connected to server');
     });
 
-    this.socket.on('playerJoined', (playerData) => {
-      this.gameEngine.addPlayer(playerData);
+    this.socket.on('disconnect', () => {
+      console.log('Disconnected from server');
     });
 
-    this.socket.on('playerLeft', (playerId) => {
-      this.gameEngine.removePlayer(playerId);
-    });
-
-    this.socket.on('playerMoved', (moveData) => {
-      this.gameEngine.updatePlayerPosition(moveData);
-    });
-
-    this.socket.on('playerAttacked', (attackData) => {
-      this.gameEngine.handleAttack(attackData);
-    });
-
-    this.socket.on('lobbyUpdate', (lobbyData) => {
-      this.updateLobby(lobbyData);
-    });
-
-    this.socket.on('gameStart', (gameData) => {
-      this.gameEngine.startGame(gameData);
-    });
+    // Add more socket event listeners here
   }
 
-  joinGame(playerData) {
-    this.socket.emit('join', playerData);
-  }
-
-  createLobby(lobbyName) {
-    this.socket.emit('createLobby', { name: lobbyName });
-  }
-
-  joinLobby(lobbyId) {
-    this.socket.emit('joinLobby', { lobbyId });
-  }
-
-  leaveLobby() {
-    this.socket.emit('leaveLobby');
-    this.lobby = null;
-  }
-
-  updateLobby(lobbyData) {
-    this.lobby = lobbyData;
-    // Notify the game engine or UI components about the lobby update
-    if (this.gameEngine.onLobbyUpdate) {
-      this.gameEngine.onLobbyUpdate(lobbyData);
-    }
-  }
-
-  startGame() {
-    if (this.lobby) {
-      this.socket.emit('startGame', { lobbyId: this.lobby.id });
-    }
-  }
-
-  sendMove(moveData) {
-    this.socket.emit('move', moveData);
-  }
-
-  sendAttack(attackData) {
-    this.socket.emit('attack', attackData);
-  }
-
-  disconnect() {
+  joinGame(gameId) {
     if (this.socket) {
-      this.socket.disconnect();
+      this.socket.emit('joinGame', { gameId });
     }
   }
 
-  getLobbyData() {
-    return this.lobby;
+  leaveGame() {
+    if (this.socket) {
+      this.socket.emit('leaveGame');
+    }
   }
+
+  // Add more multiplayer-related methods here
 }
 
 export default MultiplayerManager;
