54424eecf8918603d9712cf37ea18a9a98a145d9
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-09 03:49:47 -0500

    Update index.js to use GameStateManager

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Game from '../components/Game';
 import Menu from '../components/Menu';
 import TutorialScreen from '../components/TutorialScreen';
+import GameStateManager from '../game/GameStateManager';
+import { GAME_STATES } from '../game/constants';
 
 export default function Home() {
-  const [gameState, setGameState] = useState('menu');
+  const [gameState, setGameState] = useState(GAME_STATES.MENU);
 
-  const startGame = () => setGameState('game');
-  const showTutorial = () => setGameState('tutorial');
-  const returnToMenu = () => setGameState('menu');
+  useEffect(() => {
+    const handleStateChange = (newState) => {
+      setGameState(newState);
+    };
+
+    GameStateManager.addListener(handleStateChange);
+
+    return () => {
+      GameStateManager.removeListener(handleStateChange);
+    };
+  }, []);
+
+  const startGame = () => GameStateManager.setState(GAME_STATES.GAME);
+  const showTutorial = () => GameStateManager.setState(GAME_STATES.TUTORIAL);
+  const returnToMenu = () => GameStateManager.setState(GAME_STATES.MENU);
 
   return (
     <div className={styles.container}>
       <Head>
         <title>Burger vs. Jean</title>
         <meta name="description" content="An epic fighting game featuring Burger and Jean" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        {gameState === 'menu' && (
-          <>
-            <h1 className={styles.title}>Burger vs. Jean</h1>
-            <Menu onStartGame={startGame} onShowTutorial={showTutorial} />
-          </>
+        <h1 className={styles.title}>Burger vs. Jean</h1>
+
+        {gameState === GAME_STATES.MENU && (
+          <Menu onStartGame={startGame} onShowTutorial={showTutorial} />
         )}
 
-        {gameState === 'game' && (
+        {gameState === GAME_STATES.GAME && (
           <Game onReturnToMenu={returnToMenu} />
         )}
 
-        {gameState === 'tutorial' && (
+        {gameState === GAME_STATES.TUTORIAL && (
           <TutorialScreen onReturnToMenu={returnToMenu} />
         )}
       </main>
