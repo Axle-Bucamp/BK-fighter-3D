5aa3f456af3f681bd7edeba371f1d07cec71466c
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-10 03:38:24 -0500

    Update MainMenu component with new game mode options

--- components/MainMenu.js
+++ components/MainMenu.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import styles from '../styles/Menu.module.css';
 
-const MainMenu = ({ onStartArcade, onStartStoryMode, onStartVersus, onStartOnline, onShowOptions, onShowTutorial }) => {
+const MainMenu = ({ onGameModeSelect }) => {
   return (
     <div className={styles.menuContainer}>
       <h1 className={styles.title}>Burger King Fighter 3D</h1>
       <nav className={styles.menuNav}>
-        <button className={styles.menuButton} onClick={onStartArcade}>
+        <button className={styles.menuButton} onClick={() => onGameModeSelect('arcade')}>
           Arcade Mode
         </button>
-        <button className={styles.menuButton} onClick={onStartStoryMode}>
+        <button className={styles.menuButton} onClick={() => onGameModeSelect('story')}>
           Story Mode
         </button>
-        <button className={styles.menuButton} onClick={onStartVersus}>
+        <button className={styles.menuButton} onClick={() => onGameModeSelect('versus')}>
           Versus Mode
         </button>
-        <button className={styles.menuButton} onClick={onStartOnline}>
+        <button className={styles.menuButton} onClick={() => onGameModeSelect('multiplayer')}>
           Online Multiplayer
         </button>
-        <button className={styles.menuButton} onClick={onShowOptions}>
+        <button className={styles.menuButton} onClick={() => onGameModeSelect('options')}>
           Options
         </button>
-        <button className={styles.menuButton} onClick={onShowTutorial}>
+        <button className={styles.menuButton} onClick={() => onGameModeSelect('tutorial')}>
           Tutorial
         </button>
       </nav>
